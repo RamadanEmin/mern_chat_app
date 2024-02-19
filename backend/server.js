caa51ee68e7bb88977f5f927c76eb9646eb13530
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-19 23:44:05 +0200

    Added Sign up controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+
+app.use('/api/auth',authRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
