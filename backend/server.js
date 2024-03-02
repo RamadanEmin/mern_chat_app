e20230fe70c75676dfb2e40df751e92eafa5d39d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-02 11:07:11 +0200

    Added GET users controller

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
-import authRoutes from './routes/auth.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
+import authRoutes from './routes/auth.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,8 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
+app.use('/api/users',userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
