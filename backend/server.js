0a069ee3797a1c6c3ad2b52eea1d08115b099afc
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-03 13:52:45 +0200

    Added 'Send message' controller

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 
 import connectToMongoDB from './db/connectToMongoDB.js';
-import authRoutes from './routes/auth.routes.js';
-import userRoutes from './routes/user.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
 
 app.listen(PORT, () => {
