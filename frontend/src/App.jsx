d1e8a32bc17fcf5a50d99a7bb034e484b970ba29
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-04 13:18:56 +0200

    Added Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {  Route, Routes } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
 
 import './App.css';
@@ -12,6 +13,7 @@ function App() {
         <div className="p-4 h-screen flex items-center justify-center">
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
                 <Route path='/signup' element={<SignUp />} />
             </Routes>
             <Toaster />
