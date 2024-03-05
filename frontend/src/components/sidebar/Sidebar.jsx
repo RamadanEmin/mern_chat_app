e4b9b079c6abd02556f629da29f5f17160276cc2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-05 19:19:13 +0200

    Added 'Search input' component

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,10 @@
 import LogoutButton from './LogoutButton';
+import SearchInput from './SearchInput';
 
 const Sidebar = () => {
     return (
         <div className="border-r border-slate-500 p-4 flex flex-col">
+            <SearchInput />
             <div className="divider px-3 before:bg-sky-800 after:bg-sky-800"></div>
             <LogoutButton/>
         </div>
