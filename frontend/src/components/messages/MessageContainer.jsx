006d29057c2b363b301555682bfd5faa1ddaf819
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-06 20:23:13 +0200

    Added 'Message input' component

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,4 +1,5 @@
 import Messages from './Messages';
+import MessageInput from './MessageInput';
 
 const MessageContainer = () => {
     return (
@@ -10,6 +11,7 @@ const MessageContainer = () => {
                 </div>
 
                 <Messages />
+                <MessageInput />
             </>
         </div>
     );
