9719f67d60cae55f57e65960a4d20752fcf6a52b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-07 17:58:42 +0200

    Update Messages component

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,19 +1,28 @@
+import { useEffect, useRef } from 'react';
+import useGetMessages from '../../hooks/useGetMessages';
 import Message from './Message';
 
 const Messages = () => {
+    const { loading, messages } = useGetMessages();
+    const lastMessageRef = useRef();
+
+    useEffect(() => {
+        setTimeout(() => {
+            lastMessageRef.current?.scrollIntoView({ behaviour: 'smooth' });
+        }, 100);
+    }, [messages]);
+
     return (
         <div className="px-4 flex-1 overflow-auto">
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
-            <Message />
+            {!loading && messages.length > 0 && messages.map((message) => (
+                <div key={message._id} ref={lastMessageRef}>
+                    <Message message={message} />
+                </div>
+            ))}
+
+            {!loading && messages.length === 0 && (
+                <p className="text-center">Send a message to start the conversation</p>
+            )}
         </div>
     );
 };
